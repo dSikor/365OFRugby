d4ce870cea3b14b8b60e31652f7a785df8931881
Damian <dsikora08@gmail.com>
2021-02-04 17:45:38 +0100

    Dodanie kodu js związanego z pojawianiem się menu rozwijanego.

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+
+  
+const menuBTN = document.querySelector('.section-nav-tit__btn');
+const menuElements = document.querySelector('.menu-elements');
+
+let menuOpen = false;
+menuBTN.addEventListener('click',()=>{
+  if(!menuOpen){
+    menuBTN.classList.add('open');
+    menuElements.classList.add('show');
+    menuOpen = true;
+  }else{
+    menuBTN.classList.remove('open');
+    menuElements.classList.remove('show');
+    menuOpen = false;
+  }
+});
+
+
+
+
+
+
+
     $('.icon-one').click(function() {
       $('.icon-one').toggleClass('active-one');
     });
